6e13197e3ab5b4d9820e8a008e8d298c079ac5ea
unknown <yiii1x>
2024-03-15 11:26:18 +0800

    week04

---
+++ week04/week04-2.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+//for(int i=0;i<3;i++){ ///基礎型從0開始(電腦理解)
+    for(int i=1;i<=3;i++){ ///進階型從1開始(人類理解)
+        printf("i是:%d\n",i);
+    }
+}
---
+++ week04/week04-2.py
@@ -0,0 +1,5 @@
+for i in range(0,3):
+  print('i是',i)
+  
+ for i in range(1,1+3):
+  print('i是',i)
