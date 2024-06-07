a91c444e62c8cbbe851c133b62f9f9ce56eefc67
yiii1x <145088321+yiii1x@users.noreply.github.com>
2024-06-07 11:42:44 +0800

    week16

---
+++ week16/week16-2.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void myPrint(int a[10]){
+    for(int i=0;i<10;i++) printf("%d ",a[i]);
+    printf("\n");
+}
+int main(){
+    int a[10] = {9,8,7,6,5,4,3,2,1,0};
+    myPrint(a);
+
+    for(int i=0;i<9;i++){
+        for(int j=i+1;j<9;j++){
+            if(a[i]>a[j]){
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+        myPrint(a);
+    }
+}
